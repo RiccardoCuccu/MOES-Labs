a20c6ef1718037b454c123c538ce5ab7dd288be5
Riccardo <r.cuccu@outlook.it>
2020-10-28 18:15:50 +0100

    Update lab1

--- lab1/Exercise_2/BasicModule.cpp
+++ lab1/Exercise_2/BasicModule.cpp
@@ -1,8 +1,9 @@
 #include "BasicModule.h"
 
 void BasicModule::ex_thread_process(void) {	// implementation of member function (process)
-  // print the name of the current SystemC module instance
-  std::cout << "ex_thread_process executed within " << name() << std::endl;
-  // It is the same as 
-  // printf("ex_thread_process executed within %s\n",name().c_str());
+	// display an output that could count down from 3 to 1
+	for (int i=3; i>0; i--) {
+		std::cout << i << std::endl;
+	}
 }
+
--- lab1/Exercise_3/EX_sc_time.cpp
+++ lab1/Exercise_3/EX_sc_time.cpp
@@ -6,31 +6,17 @@ using std::endl;
 #include "EX_sc_time.h"
 
 void EX_sc_time::time_thread(void) {
-  cout << "INFO: Time resolution is " << sc_get_time_resolution() << endl;
-  const sc_time t_1FS(1,SC_FS);
-  const sc_time t_1S(1,SC_SEC);
-  const sc_time t_1HR(3600*t_1S);// computed time
-  sc_time t_DELAY(t_1FS);
-  // Delay by powers of ten and display the resulting time
-  for (;t_DELAY<=2*t_1HR;t_DELAY*=10) {
-    cout << "INFO: Delaying " << t_DELAY << " ";
-    wait(t_DELAY);
-    cout << "INFO: Time is " << sc_time_stamp() << "!" << endl;
-  }//endfor
-  for (t_DELAY=2*t_1HR;t_DELAY>=t_1FS;t_DELAY/=10) {
-    cout << "INFO: Delaying " << t_DELAY << " ";
-    wait(t_DELAY);
-    cout << "INFO: Time now " << sc_time_stamp() << endl;
-  }//endfor
-  wait(SC_ZERO_TIME);
-  cout << "INFO: wait(SC_ZERO_TIME)" << endl;
-  cout << "INFO: Time still " << sc_time_stamp() << endl;
-  #ifdef USE_STOP
-  cout << "INFO: Simulation will stop at the current time." << endl;
-  sc_stop();
-  #else
-  cout << "INFO: Simulation will now proceed to limit specified by sc_start()" << endl;
-  cout << "----: which is \"the maximum simulation time allowed\"." << endl;
-  #endif
-}//end time_thread()
+	//cout << "INFO: Time resolution is " << sc_get_time_resolution() << endl;
+	const sc_time t_20NS(20,SC_NS);
 
+	sc_time t_DELAY(t_20NS);
+
+	for (int i=20; i>0; i--) {
+		wait(t_DELAY);
+		//std::cout << i << "\t=>\t" << sc_time_stamp() << std::endl;
+		std::cout << i << std::endl;
+	}
+
+	//cout << "End" << endl;
+
+} //end time_thread()
