9d5a0ef6de1099368892c3defdefb746e5d32571
Riccardo <r.cuccu@outlook.it>
2022-02-20 19:57:41 +0100

    Update Exercise_1

--- labs/lab4/Exercise_1/testbench.h
+++ labs/lab4/Exercise_1/testbench.h
@@ -1,11 +1,11 @@
 #ifndef TESTBENCH_H
 #define TESTBENCH_H
 
-SC_MODULE( Testbench ) 
+SC_MODULE(Testbench)
 {
 	public :
-	sc_out<bool> car ;
-	sc_in<color> Main_road , Secondary_road ;
+	sc_out<bool> car;
+	sc_in<color> Main_road , Secondary_road;
 	
 	SC_CTOR(Testbench)
 	: car("car"), Main_road("Main"), Secondary_road("Secondary") {
@@ -16,9 +16,14 @@ SC_MODULE( Testbench )
 		
 		SC_METHOD(CAR_CHECK);
 		sensitive << car;
-	} 
+
+		SC_THREAD(MONITOR_1);
+		SC_THREAD(MONITOR_2);
+		SC_THREAD(MONITOR_3);
+	}
+	
 	void INPUT_GEN ( )
-	{ 
+	{
 		car.write(false);
 		wait(5 , SC_SEC);
 		car.write(true);
@@ -31,19 +36,90 @@ SC_MODULE( Testbench )
 		car.write(false);
 		wait(30 , SC_SEC);
 		sc_stop();
-	} 
-	void OUTPUT_CHECK() 
-	{ 
-	    static const char *table[]= {"RED","YELLOW","GREEN"};
-	    cout << "@" << sc_time_stamp() << " Main      road " << table[Main_road.read()] << std::endl;
-	    cout << "@" << sc_time_stamp() << " Secondary road " << table[Secondary_road.read()] << std::endl;
-	    cout << std::endl;
 	}
+	
+	void OUTPUT_CHECK()
+	{
+		static const char *table[]= {"RED","YELLOW","GREEN"};
+		cout << "@" << sc_time_stamp() << " Main      road " << table[Main_road.read()] << std::endl;
+		cout << "@" << sc_time_stamp() << " Secondary road " << table[Secondary_road.read()] << std::endl;
+		cout << std::endl;
+	}
+	
 	void CAR_CHECK() 
 	{ 
-	    cout << "@" << sc_time_stamp() << " car " << (car ? "arrives" : "leaves") << std::endl;
-	    cout << std::endl;
+		cout << "@" << sc_time_stamp() << " car " << (car ? "arrives" : "leaves") << std::endl;
+		cout << std::endl;
 	}
+
+	/* Check if when one traffic light is green the other is red */
+
+	void MONITOR_1() {
+		static const char *table[]= {"RED","YELLOW","GREEN"};
+		while(1) {
+			wait(Main_road.value_changed_event() | Secondary_road.value_changed_event());
+			if (strcmp(table[Main_road.read()],"GREEN")==0) {
+				if (strcmp(table[Secondary_road.read()],"RED")!=0) {
+					cout << "@" << sc_time_stamp() << " Main      road " << table[Main_road.read()] << " and Secondary road " << table[Secondary_road.read()] << endl;
+					cout << std::endl;
+				}
+			} else if (strcmp(table[Secondary_road.read()],"GREEN")==0) {
+				if (strcmp(table[Main_road.read()],"RED")!=0) {
+					cout << "@" << sc_time_stamp() << " Main      road " << table[Main_road.read()] << " and Secondary road " << table[Secondary_road.read()] << endl;
+					cout << std::endl;
+				}
+			}
+		}
+	}
+	
+	/* Check if the minimum duration of any green light is 10 seconds */
+	
+	void MONITOR_2() {
+		static const char *table[]= {"RED","YELLOW","GREEN"};
+		wait(SC_ZERO_TIME);
+		while(1) {	
+			wait(Main_road.value_changed_event() | Secondary_road.value_changed_event());
+			if (strcmp(table[Main_road.read()],"GREEN")==0) {
+				wait(10, SC_SEC);
+				if (strcmp(table[Main_road.read()],"GREEN")!=0) {
+					cout << "@" << sc_time_stamp() << " Main      road GREEN for less than 10 seconds" << endl;
+					cout << std::endl;
+				}
+			} else if (strcmp(table[Secondary_road.read()],"GREEN")==0) {
+				wait(10, SC_SEC);
+				if (strcmp(table[Secondary_road.read()],"GREEN")!=0) {
+					cout << "@" << sc_time_stamp() << " Secondary road GREEN for less than 10 seconds" << endl;
+					cout << std::endl;
+				}
+			}
+		}
+	}
+	
+	/* Check if the maximum duration of any yellow light is 5 seconds */
+	
+	void MONITOR_3() {
+		static const char *table[]= {"RED","YELLOW","GREEN"};
+		wait(SC_ZERO_TIME);
+		while(1) {	
+			wait(Main_road.value_changed_event() | Secondary_road.value_changed_event());
+			if (strcmp(table[Main_road.read()],"YELLOW")==0) {
+				wait(5, SC_SEC);
+				wait(SC_ZERO_TIME);
+				if (strcmp(table[Main_road.read()],"YELLOW")==0) {
+					cout << "@" << sc_time_stamp() << " Main      road YELLOW for more than 5 seconds" << endl;
+					cout << std::endl;
+				}
+			} else if (strcmp(table[Secondary_road.read()],"YELLOW")==0) {
+				wait(5, SC_SEC);
+				wait(SC_ZERO_TIME);
+				if (strcmp(table[Secondary_road.read()],"YELLOW")==0) {
+					cout << "@" << sc_time_stamp() << " Secondary road YELLOW for more than 5 seconds" << endl;
+					cout << std::endl;
+				}
+			}
+		}
+	}
+
 };
 
 #endif
--- labs/lab4/Exercise_1/tlc.h
+++ labs/lab4/Exercise_1/tlc.h
@@ -1,36 +1,35 @@
 #ifndef TLC_H
 #define TLC_H
 
-enum color{ RED , YELLOW , GREEN };
+enum color{RED, YELLOW, GREEN};
 SC_MODULE(TLC)
-{ 
+{
 	public :
-	sc_in<bool> car ;
-	sc_out<color> M , S ;
-	SC_CTOR(TLC)  // Constructor
+	sc_in<bool> car;
+	sc_out<color> M, S;
+	SC_CTOR(TLC)						// constructor
 	: car("car"), M("Main"), S("Secondary") {
-		SC_THREAD( TLC_run ) ;
-	} 
-	void TLC_run ( ) 
-	{ 
-		M.write(GREEN); // initialization
-		S.write(RED);   
+		SC_THREAD(TLC_run);
+	}
+	void TLC_run()
+	{
+		M.write(GREEN);					// initialization
+		S.write(RED);
 		while (1) 
-		{ 	
-			wait (10 , SC_SEC);
-			if (!car) // wait until car arrives at Secondary
-			    wait(car.posedge_event()) ;
-			wait(5,SC_SEC);  
+		{	
+			wait (10, SC_SEC);
+			if (!car) wait(car.posedge_event());	// wait until car arrives at Secondary
+			wait(5, SC_SEC);
 			M.write(YELLOW);
-			wait (5,SC_SEC);
+			wait (5, SC_SEC);
 			M.write(RED);
 			S.write(GREEN);
-			wait(10,SC_SEC);
+			wait(10, SC_SEC);
 			S.write(YELLOW);
-			wait(5,SC_SEC);
+			wait(5, SC_SEC);
 			S.write(RED);
 			M.write(GREEN);
-		} 
+		}
 	}
 };
 
